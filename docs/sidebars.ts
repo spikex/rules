1dea33f72b3b7d46bea1fbc23e7b32086a836b11
Nate <sestinj@gmail.com>
2025-06-24 15:57:41 -0700

    move docs to mintlify

--- docs/sidebars.ts
+++
@@ -1,6 +0,0 @@
-import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
-
-// Empty sidebars configuration - we don't want a sidebar
-const sidebars: SidebarsConfig = {};
-
-export default sidebars;
